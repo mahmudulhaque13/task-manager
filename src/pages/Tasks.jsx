75971b8b9c1fdf389bbe1bc2c728693256244b17
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-03-13 00:54:44 +0600

    Display tasks list

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import tasks from "../data/tasks";
 
-const Tasks = () => {
-  return <div></div>;
-};
+function Tasks() {
+  return (
+    <div>
+      <h2>Tasks</h2>
+
+      {tasks.map((task) => (
+        <p key={task.id}>{task.title}</p>
+      ))}
+    </div>
+  );
+}
 
 export default Tasks;
