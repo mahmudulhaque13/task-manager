66df52a67558ec64f68c3f968fa054b92c5b5d88
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-03-13 00:56:30 +0600

    Added task details page

--- src/pages/TaskDetails.jsx
+++ src/pages/TaskDetails.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { useParams } from "react-router-dom";
+import tasks from "../data/tasks";
 
-const TaskDetails = () => {
-  return <div></div>;
-};
+function TaskDetails() {
+  const { id } = useParams();
+
+  const task = tasks.find((t) => t.id === Number(id));
+
+  if (!task) {
+    return <h2>Task not found</h2>;
+  }
+
+  return (
+    <div>
+      <h2>{task.title}</h2>
+      <p>Status: {task.completed ? "Done" : "Pending"}</p>
+    </div>
+  );
+}
 
 export default TaskDetails;
