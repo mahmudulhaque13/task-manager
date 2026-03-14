23bdbc3e6274f446f71ea63eb616fdd57748fa50
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-03-14 23:23:51 +0600

    Add new task feature

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,10 +1,30 @@
 import tasks from "../data/tasks";
 import { Link } from "react-router-dom";
 
+const [title, setTitle] = useState("");
+
+function handleAdd() {
+  const newTask = {
+    id: Date.now(),
+    title: title,
+    completed: false,
+  };
+
+  setTasks([...tasks, newTask]);
+  setTitle("");
+}
+
 function Tasks() {
   return (
     <div>
       <h2>All Tasks</h2>
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="New task"
+      />
+
+      <button onClick={handleAdd}>Add Task</button>
 
       {tasks.map((task) => (
         <div key={task.id}>
