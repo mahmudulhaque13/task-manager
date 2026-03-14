02da76211bf7f910b387864c2577bb91538d1a37
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-03-14 23:11:33 +0600

    Display tasks with details link

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,12 +1,17 @@
 import tasks from "../data/tasks";
+import { Link } from "react-router-dom";
 
 function Tasks() {
   return (
     <div>
-      <h2>Tasks</h2>
+      <h2>All Tasks</h2>
 
       {tasks.map((task) => (
-        <p key={task.id}>{task.title}</p>
+        <div key={task.id}>
+          <h3>{task.title}</h3>
+
+          <Link to={`/tasks/${task.id}`}>View Details</Link>
+        </div>
       ))}
     </div>
   );
