1af56e1bd988b5047a20790931a9879f6413c301
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-03-15 00:38:02 +0600

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,39 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+
+import Navbar from "./components/Navbar";
 
 import Home from "./pages/Home";
 import Tasks from "./pages/Tasks";
 import TaskDetails from "./pages/TaskDetails";
 import Completed from "./pages/Completed";
 import NotFound from "./pages/NotFound";
-import Navbar from "./components/Navbar";
 
-import { useState } from "react";
 import tasksData from "./data/tasks";
 
-const [tasks, setTasks] = useState(tasksData);
+function App() {
+  const [tasks, setTasks] = useState(tasksData);
 
-export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
 
-        <Route path="/tasks" element={<Tasks tasks={tasks} />} />
+        <Route
+          path="/tasks"
+          element={<Tasks tasks={tasks} setTasks={setTasks} />}
+        />
 
-        <Route path="/tasks/:id" element={<TaskDetails />} />
+        <Route path="/tasks/:id" element={<TaskDetails tasks={tasks} />} />
 
-        <Route path="/completed" element={<Completed />} />
+        <Route path="/completed" element={<Completed tasks={tasks} />} />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+export default App;
