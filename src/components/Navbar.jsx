1046505586e7a764e9976a567758e0383009ee35
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-03-16 23:39:30 +0600

    Added task counters in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,17 @@
 import { Link } from "react-router-dom";
 
-function Navbar() {
+function Navbar({ tasks }) {
+  const completed = tasks.filter((t) => t.completed).length;
+
+  const pending = tasks.filter((t) => !t.completed).length;
+
   return (
     <nav>
       <Link to="/">Home</Link> |<Link to="/tasks">Tasks</Link> |
       <Link to="/completed">Completed</Link>
+      <p>Total: {tasks.length}</p>
+      <p>Completed: {completed}</p>
+      <p>Pending: {pending}</p>
     </nav>
   );
 }
